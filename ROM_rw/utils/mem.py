6ad7d99a2d38a2f850277517abb5c40c4dc2e045
mintRaven <debjeetbanerjee48@gmail.com>
2025-06-16 08:16:25 +0530

    [FIX]: IROM handlers and firmware image manupulators block (BOOT)

---
+++ ROM_rw/utils/mem.py
@@ -0,0 +1,71 @@
+from ..image import ESP32FirmwareImage, ESPFirmwareImage, OTAFirmwareImage
+from ..loader import ESP32ROM, ESPBOOTLOADER, ESPLoader
+import struct
+import sys
+from . import FatalError, div_roundup
+from . import DETECTED_FLASH_SIZES
+def LoadFirmwareImage(chip, filename):  
+    with open(filename, 'rb') as f:
+        if chip == 'esp32':
+            return ESP32FirmwareImage(f)
+        else:
+            magic = ord(f.read(1))
+            f.seek(0)
+            if magic == ESPLoader.ESP_IMAGE_MAGIC:
+                return ESPFirmwareImage(f)
+            elif magic == ESPBOOTLOADER.IMAGE_V2_MAGIC:
+                return OTAFirmwareImage(f)
+            else:
+                raise FatalError("Invalid image magic number: %d" % magic)
+
+def load_ram(esp, args):
+    image = LoadFirmwareImage(esp, args.filename)
+
+    print('RAM boot...')
+    for (offset, size, data) in image.segments:
+        print('Downloading %d bytes at %08x...' % (size, offset), end=' ')
+        sys.stdout.flush()
+        esp.mem_begin(size, div_roundup(size, esp.ESP_RAM_BLOCK), esp.ESP_RAM_BLOCK, offset)
+
+        seq = 0
+        while len(data) > 0:
+            esp.mem_block(data[0:esp.ESP_RAM_BLOCK], seq)
+            data = data[esp.ESP_RAM_BLOCK:]
+            seq += 1
+        print('done!')
+
+    print('All segments done, executing at %08x' % image.entrypoint)
+    esp.mem_finish(image.entrypoint)
+
+def read_mem(esp, args):
+    print('0x%08x = 0x%08x' % (args.address, esp.read_reg(args.address)))
+
+
+def write_mem(esp, args):
+    esp.write_reg(args.address, args.value, args.mask, 0)
+    print('Wrote %08x, mask %08x to %08x' % (args.value, args.mask, args.address))
+
+
+def dump_mem(esp, args):
+    f = open(args.filename, 'wb')
+    for i in range(args.size // 4):
+        d = esp.read_reg(args.address + (i * 4))
+        f.write(struct.pack(b'<I', d))
+        if f.tell() % 1024 == 0:
+            print('\r%d bytes read... (%d %%)' % (f.tell(),
+                                                  f.tell() * 100 // args.size),
+                  end=' ')
+        sys.stdout.flush()
+    print('Done!')
+
+
+def detect_flash_size(esp, args):
+    if args.flash_size == 'detect':
+        flash_id = esp.flash_id()
+        size_id = flash_id >> 16
+        args.flash_size = DETECTED_FLASH_SIZES.get(size_id)
+        if args.flash_size is None:
+            print('Warning: Could not auto-detect Flash size (FlashID=0x%x, SizeID=0x%x), defaulting to 4MB' % (flash_id, size_id))
+            args.flash_size = '4MB'
+        else:
+            print('Auto-detected Flash size:', args.flash_size)
